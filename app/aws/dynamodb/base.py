6e4dfb38e87d98bbaf7a4268feae4c23601ed019
Ryo H <49798519+ryobs@users.noreply.github.com>
2021-10-16 15:47:08 +0900

    JSON文字列に変換する処理を関数化

--- app/aws/dynamodb/base.py
+++ app/aws/dynamodb/base.py
@@ -6,27 +6,29 @@ import json
 class Base():
     """エンティティ基底クラス"""
 
-    def __str__(self) -> str:
-        return json.dumps(self.as_json_dict(),
-                          ensure_ascii=False, sort_keys=True)
+    def __str__(self):
+        return self.to_json_string()
+
+    def __repr__(self):
+        return str(self)
+
+    def to_json_string(self) -> str:
+        """JSON文字列に変換"""
+        return json.dumps(self.to_dict(), ensure_ascii=False, sort_keys=True)
 
     def to_dict(self) -> dict:
         """dict型に変換"""
-        return self.as_json_dict()
-
-    def as_json_dict(self) -> dict:
         data = {}
         for key, value in self.__dict__.items():
             if isinstance(value, (list, tuple, set)):
                 data[key] = list()
                 for item in value:
-                    if hasattr(item, 'as_json_dict'):
-                        data[key].append(item.as_json_dict())
+                    if hasattr(item, "to_dict"):
+                        data[key].append(item.to_dict())
                     else:
                         data[key].append(item)
-
-            elif hasattr(value, 'as_json_dict'):
-                data[key] = value.as_json_dict()
+            elif hasattr(value, "to_dict"):
+                data[key] = value.to_dict()
             elif value is not None:
                 data[key] = value
 
